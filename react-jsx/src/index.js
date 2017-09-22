0caf915449cb13455d045021a8da5fcbd5088aa5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-22 18:47:48 +0200

    virtual dom practise

--- react-jsx/src/index.js
+++ react-jsx/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import sports from './data/sports.json'
 
 
+ReactDOM.render(<ul>
 
-ReactDOM.render(<p>siema</p>, document.getElementById('root'));
+    {sports.map(item => {
+        return(
+
+            <li key={item.id}>
+                <strong>{item.name}</strong><br/>
+                <strong>{item.surname}</strong><br/>
+                <strong>{item.sport}</strong><br/>
+                <strong>{item.club}</strong><br/><br/>
+            </li>
+        )
+
+    })}
+</ul>, document.getElementById('root'));
 
