1cede0082a33d324061c7dc8b542ab21ee607f25
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-22 20:45:13 +0200

    export code to out function

--- react-jsx/src/index.js
+++ react-jsx/src/index.js
@@ -4,28 +4,36 @@ import ReactDOM from 'react-dom';
 import sports from './data/sports.json'
 
 
-ReactDOM.render(<ul>
 
-    {sports.map(item => {
+const players = () => {
 
-        const date  = new Date(item.date);  // parsowania ze stringa na date
+return (
+    <ul>
 
-        if (date >= Date.now()) {
+        {sports.map(item => {
 
-            return(
+            const date  = new Date(item.date);  // parsowania ze stringa na date
 
-                <li key={item.id}>
-                    <strong>{item.name}</strong><br/>
-                    <strong>{item.surname}</strong><br/>
-                    <strong>{item.sport}</strong><br/>
-                    <strong>{item.club}</strong><br/>
-                    <strong>{item.date}</strong><br/><br/>
-                </li>
-            )
+            if (date >= Date.now()) {
+
+                return(
+
+                    <li key={item.id}>
+                        <strong>{item.name}</strong><br/>
+                        <strong>{item.surname}</strong><br/>
+                        <strong>{item.sport}</strong><br/>
+                        <strong>{item.club}</strong><br/>
+                        <strong>{item.date}</strong><br/><br/>
+                    </li>
+                )
 
 
-        }
-        return null;
-    })}
-</ul>, document.getElementById('root'));
+            }
+            return null;
+        })}
+    </ul>
+);
+};
+
+ReactDOM.render(players(), document.getElementById('root'));
 
